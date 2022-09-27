7b444ac04d3ea32c8027cbf6d56e4243e6cee822
Cusnir Nichita <Cusnir.Ni.2022@skola.ssps.cz>
2022-09-27 09:45:49 +0200

    init

---
+++ source/repos/2. Hodina/2. Hodina/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.Hodina
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double cislo1, cislo2, vysledek = 0;
+            string operace;
+
+            cislo1 = double.Parse(Console.ReadLine());
+            cislo2 = double.Parse(Console.ReadLine());
+            operace = Console.ReadLine();
+
+
+            if (operace == "+")
+            {
+                vysledek = cislo1 + cislo2;
+            }
+            else if (operace == "-")
+            {
+                vysledek = cislo1 - cislo2;
+            }
+            else if (operace == "*")
+            {
+                vysledek = cislo1 *  cislo2;
+            }
+            else if (operace == "/")
+            {
+                vysledek = cislo1 / cislo2;
+            }
+            else
+            {
+                Console.WriteLine("Wrong Input");
+            }
+            Console.ReadKey();
+        }
+    }
+}
